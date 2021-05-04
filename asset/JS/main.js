0476728ce139884b539460a8a2b12575a7861679
planeset94 <planeset94@gmail.com>
2021-05-04 22:52:45 +0200

    Html: add hamburger img. JS: add price

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -1,6 +1,7 @@
 var ingredEl = document.querySelector("#ingredients");
 var calcPriceEl = document.getElementById('calc_price');
 var discountEl = document.getElementById('discount');
+var priceElement = document.getElementById('price');
 // INSERISCO I VARI INGREDIENTEI IN UNA MATRICE
 var list = [
     ['Cheese', '1'],
@@ -14,10 +15,12 @@ var list = [
 
 var i = 0;
 while (i < list.length) {
+    // problema nella visualizzazione delle immagini 
     ingredEl.innerHTML +=
         `
     <li class="list-none half_flex-basis">
-        <img class="food-emoticon" src="./asset/img/"${list[i][0]}".svg">
+        
+        <img class="food-emoticon" src="./asset/img/"+(list[i][0])+".svg">
         <label class="mx-1" for="${list[i][0]}">${list[i][0]}</label>
         <input type="checkbox" data-price="${list[i][1]}" id="${list[i][0]}">
         <span class="ml-05">Add</span>
@@ -38,7 +41,7 @@ function isChecked(element) {
 
 }
 
-// CREO UN CICLO 
+// CREO UN CICLO PER STABILIRE IL PREZZO DEGLI INGREDIENTI 
 calcPriceEl.addEventListener('click', function () {
 
     var price;
@@ -48,10 +51,12 @@ calcPriceEl.addEventListener('click', function () {
     var averageDiscount;
     var j = 0;
     var additionPrice = 0;
+    // TRAMITE IL CICLO while, individuo gli elementi stampati su HTML e verifico che siano stati fleggati
     while (j < list.length) {
-        var x = document.getElementById(list[j][0]);
-        if (isChecked(x)) {
-            additionPrice += Number(x.getAttribute('data-price'));
+        var xElement = document.getElementById(list[j][0]);
+        if (isChecked(xElement)) {
+            // additionPrice += Number(xElement.getAttribute('data-price'));
+            additionPrice += Number(list[j][1]);
             console.log(additionPrice);
         }
 
@@ -73,6 +78,6 @@ calcPriceEl.addEventListener('click', function () {
     } else {
         alert('ERROR');
     }
-    console.log(price);
 
+    priceElement.innerHTML = price.toFixed(2) + " €";
 });
